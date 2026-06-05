822fa5a14fdfd90adfa3115b8df86dad54862741
ThomasEcalle <thomasecalle@gmail.com>
2026-06-05 16:16:23 +0200

    End of constructors chapter

--- lib/constructors/constructors.dart
+++ lib/constructors/constructors.dart
@@ -1,10 +1,21 @@
 class Toto {
-  final String name;
+  final String lastName;
+  final String firstName;
+  final int age;
+  final String address;
+  final Toto? friend;
 
-  Toto(this.name);
+  const Toto(this.lastName, {this.firstName = '', this.age = 50, required this.address, this.friend});
 }
 
 void main() {
-  final toto1 = Toto('Salut');
-  print(toto1.name);
+  const toto1 = Toto('Ecalle', age: 20, firstName: 'Thomas', address: 'Address 1');
+  const toto2 = Toto('Salut', address: 'Address 2');
+  const toto3 = Toto('Salut', address: 'Address 3');
+
+  print(toto3.friend?.firstName ?? 'Cas par défaut');
+
+  // print(toto1.age);
+  // print(toto2.age);
+  // print(toto2 == toto3);
 }
