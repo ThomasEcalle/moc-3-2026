324538c81a859d18b95361308b1622a5a7dfa1f3
ThomasEcalle <thomasecalle@gmail.com>
2026-06-29 16:54:18 +0200

    Add ListView example

--- lib/lists/lists_screen.dart
+++ lib/lists/lists_screen.dart
@@ -18,21 +18,45 @@ class ListsScreen extends StatelessWidget {
         backgroundColor: Colors.amber,
         title: Text('Lists Screen'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: List.generate(users.length, (index) {
-            final user = users[index];
-            print('Building user: ${user.firstName} ${user.lastName}');
-            return ListTile(
-              leading: CircleAvatar(
-                child: Text(user.firstName[0]),
-              ),
-              title: Text('${user.firstName} ${user.lastName}'),
-              subtitle: Text('User ID: $index'),
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: List.generate(users.length, (index) {
+      //       final user = users[index];
+      //       print('Building user: ${user.firstName} ${user.lastName}');
+      //       return ListTile(
+      //         leading: CircleAvatar(
+      //           child: Text(user.firstName[0]),
+      //         ),
+      //         title: Text('${user.firstName} ${user.lastName}'),
+      //         subtitle: Text('User ID: $index'),
+      //       );
+      //     }),
+      //   ),
+      // ),
+      body: ListView.separated(
+        itemCount: users.length + 1,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, index) {
+          if(index == users.length) {
+            return const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Center(child: CircularProgressIndicator()),
             );
-          }),
-        ),
+          }
+
+          final user = users[index];
+          print('Building user: ${user.firstName} ${user.lastName}');
+          return ListTile(
+            leading: CircleAvatar(child: Text(user.firstName[0])),
+            title: Text('${user.firstName} ${user.lastName}'),
+            subtitle: Text('User ID: $index'),
+          );
+        },
       ),
     );
   }
 }
+
+
+
+
